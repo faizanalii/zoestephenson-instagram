12d1865e0da6924a9eb5034a42949f6f7062b43c
faizanalii <ch.faizan15a@gmail.com>
2026-04-29 10:44:53 +0200

    hit the main URL not the redirected one

--- scraper/src/comment_scraper/post_page.py
+++ scraper/src/comment_scraper/post_page.py
@@ -27,6 +27,10 @@ async def get_post_page(post_url: str, proxy: str, cookies: dict[str, str]) -> s
 
     # Remove the query parameters from the post URL to avoid issues with fetching the page
     post_url = post_url.split("?")[0]
+    # If reel in the URL and not /reels/ then replace it with /reels/ to ensure we correctly identify it as a reel URL in the _build_post_from_json_scripts function
+
+    if "/reel/" in post_url and "/reels/" not in post_url:
+        post_url = post_url.replace("/reel/", "/reels/")
 
     response_obj = await client.get(
         post_url, version=Version.HTTP_2, allow_redirects=True, cookies=cookies
